777df5de56ac474c0ebb9f28bbd19f0c1bcdaccd
ventenni <ventenni@gmail.com>
2020-05-25 15:40:05 +1000

    created blog components

---
+++ pages/posts/first-post.js
@@ -0,0 +1,22 @@
+// Next Components
+import Head from 'next/head';
+import Link from 'next/link';
+
+// Layout
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+	 <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
